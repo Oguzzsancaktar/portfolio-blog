a11c75c10a07c8b6b5a5c06dbbed5efb2833f476
oguzzsancaktar <oguztahasancaktar@gmail.com>
2022-12-11 14:31:53 +0300

    pug basics added

---
+++ app.js
@@ -0,0 +1,14 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => res.render('pages/home'))
+app.get('/about', (req, res) => res.render('pages/about'))
+app.get('/collections', (req, res) => res.render('pages/collections'))
+app.get('/detail/:id', (req, res) => res.render('pages/detail'))
+
+app.listen(port, () => console.log(`Example app l istening on port ${port}!`))
