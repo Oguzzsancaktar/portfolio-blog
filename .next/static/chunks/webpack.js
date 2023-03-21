ed67edd4bb063d58e4bff3798bd2b3af699d6b5f
Oğuz Taha Sancaktar <46040028+Oguzzsancaktar@users.noreply.github.com>
2023-03-21 19:02:37 +0300

    Public folder pushed just for now

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cf4c484011d63fd7"; }
+/******/ 		__webpack_require__.h = function() { return "5553ffbb9cb07db2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
